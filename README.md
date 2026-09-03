17d09c0545469953fd20a5d46a4f29ed2f67d1aa
Yehor Oblyvantsov <xineraman8@gmail.com>
2026-09-03 13:44:13 +0200

    Cloned

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repository to Showcase Version Control
+
+This is a ReadMe file
---
+++ basic-calc.py
@@ -0,0 +1,12 @@
+#Basic 2-number Calculator Functions
+def add(x,y):
+	pass
+
+def multiply(x,y):
+	pass
+
+def subtract(x,y):
+	pass
+
+def divide(x,y):
+	pass
